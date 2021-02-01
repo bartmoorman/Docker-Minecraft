0ff3e243f388a84b97a9005b486a5bd85282db9c
Bart Moorman <github@id10t.us>
2021-01-31 17:08:26 -0700

    Update java opts - should help with performance https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -131,9 +131,24 @@ $(which java) \
     -Dserver.name=${MC_SERVER_NAME:-minecraft} \
     -Xms${MC_MIN_MEM:-1G} \
     -Xmx${MC_MAX_MEM:-2G} \
-    -XX:+UseConcMarkSweepGC \
-    -XX:+CMSIncrementalPacing \
-    -XX:+AggressiveOpts \
+    -XX:+UseG1GC \
+    -XX:+ParallelRefProcEnabled \
+    -XX:MaxGCPauseMillis=200 \
+    -XX:+UnlockExperimentalVMOptions \
+    -XX:+DisableExplicitGC \
+    -XX:+AlwaysPreTouch \
+    -XX:G1NewSizePercent=30 \
+    -XX:G1MaxNewSizePercent=40 \
+    -XX:G1HeapRegionSize=8M \
+    -XX:G1ReservePercent=20 \
+    -XX:G1HeapWastePercent=5 \
+    -XX:G1MixedGCCountTarget=4 \
+    -XX:InitiatingHeapOccupancyPercent=15 \
+    -XX:G1MixedGCLiveThresholdPercent=90 \
+    -XX:G1RSetUpdatingPauseTimePercent=5 \
+    -XX:SurvivorRatio=32 \
+    -XX:+PerfDisableSharedMem \
+    -XX:MaxTenuringThreshold=1 \
     ${MC_JAVA_ARGS} \
     -jar /opt/minecraft/spigot-*.jar \
     nogui &
